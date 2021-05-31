1a5763ed4c954f4107e79c0e3ae66bebf3d8d6f6
Garciasebas <sebasgara@hotmail.com>
2021-05-31 19:59:16 -0400

    Segunda Entrega

--- main.py
+++ main.py
@@ -1,81 +1,76 @@
-# This is a sample Python script.
-import tabula
 import os
-from csv import reader
 from pathlib import Path
+from natsort import natsorted
+import pandas as pd
+import tabula
+import time
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-class Persona:
-    def __init__(self, ci, nombre, monto):
-        self.ci = ci
-        self.nombre = nombre
-        self.monto = monto
+def imprimir_csv(path_archivo, df_list):
+    df = pd.concat(df_list, axis=0, ignore_index=True)
+    new_df = df.iloc[:, [0, 1, 3, 4, 6, 7]]
+    new_df.columns = ['Ci', 'Nombre', 'Monto', 'Concepto', 'Cargo', 'Ingreso']
+    new_df.to_csv(path_archivo, index=False)
 
-def leer_csv(nombre_archivo):
-    with open('./tabula-csv-out/' + nombre_archivo + '.csv', 'r') as read_obj:
-        csv_reader = reader(read_obj)
-        header = next(csv_reader)
-        # Check file as empty
-        if header != None:
-            # Iterate over each row after the header in the csv
-            list_persona = []
-            for row in csv_reader:
-                # row variable is a list that represents a row in csv
-                # print(row)
-                if(row[0] == ""):
-                    cinombre = row[1].split(" ",1)
-                    monto = row[4].split(" ",1)[0]
-                    persona = Persona(cinombre[0], cinombre[1], monto)
-                else:
-                    cinombre = row[0].split(" ", 1)
-                    monto = row[3].split(" ", 1)[0]
-                    persona = Persona(cinombre[0], cinombre[1], monto)
-                list_persona.append(persona)
-            convert(list_persona, './csv-final/' + nombre_archivo + '.csv')
 
-def convert(list_persona, outfile):
-    with open(outfile, 'w') as f:
-        f.write('Ci;Nombre;Monto')
-        f.write('\n')
-        for persona in list_persona:
-            f.write(persona.ci + ';' + persona.nombre + ';' + persona.monto)
-            f.write('\n')
+def extraer_datos(path_archivo, indice):
+    if indice == 1:
+        #estos valores salen del photogimp, notacion point
+        area = [100.6, 56.9, 528.5, 767.5]
+    else:
+        area = [63.4, 56.9, 525.6, 767.5]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    return tabula.read_pdf(path_archivo, pages='all', guess=False, area=area,
+                           columns=[92.6, 255.4, 299.8, 335.8, 462.2, 624.0, 720.7], pandas_options={'header': None})
+
+
+def procesar_archivos():
+    print(f'Inicio de procesamiento de archivos\n')
+
     files = os.listdir('./pdfs')
     for nombre_archivo in files:
+        start = time.time()
+        print(f'Procesando archivo: {nombre_archivo}\n')
         nombre_pdf = nombre_archivo.split('.')[0]
-        nombre_carpeta_pdf_parts = './pdf-parts/' + nombre_pdf
-        nombre_carpeta_jpeg = './jpeg/' + nombre_pdf
-        nombre_carpeta_jpeg_pdf = './jpeg-pdf/' + nombre_pdf
-        nombre_carpeta_pdf_searchable = './pdf-searchable/' + nombre_pdf
-        nombre_carpeta_tabula_csv_out = './tabula-csv-out/' + nombre_pdf
-        nombre_carpeta_csv_final = './csv-final/' + nombre_pdf
+        nombre_carpeta_pdf_parts = f'./pdf-parts/{nombre_pdf}'
+        nombre_carpeta_jpeg = f'./jpeg/{nombre_pdf}'
+        nombre_carpeta_jpeg_pdf = f'./jpeg-pdf/{nombre_pdf}'
+        nombre_carpeta_pdf_searchable = f'./pdf-searchable/{nombre_pdf}'
+        nombre_carpeta_csv_final = f'./csv-final'
         Path(nombre_carpeta_pdf_parts).mkdir(parents=True, exist_ok=True)
         Path(nombre_carpeta_jpeg).mkdir(parents=True, exist_ok=True)
         Path(nombre_carpeta_jpeg_pdf).mkdir(parents=True, exist_ok=True)
         Path(nombre_carpeta_pdf_searchable).mkdir(parents=True, exist_ok=True)
-        Path(nombre_carpeta_tabula_csv_out).mkdir(parents=True, exist_ok=True)
         Path(nombre_carpeta_csv_final).mkdir(parents=True, exist_ok=True)
-        os.system('pdfseparate ./pdfs/' + nombre_pdf + '.pdf ' + nombre_carpeta_pdf_parts + '/part-%d')
-        files = os.listdir(nombre_carpeta_pdf_parts)
-        for f in files:
-            print(f)
-            print('pdftoppm')
-            os.system('pdftoppm -singlefile -jpeg -r 600 -jpegopt quality=100 ' + nombre_carpeta_pdf_parts + '/' + f + ' ' + nombre_carpeta_jpeg + '/' + f)
-            print('img2pdf')
-            os.system('img2pdf ' + nombre_carpeta_jpeg + '/' + f + '.jpg -o ' + nombre_carpeta_jpeg_pdf + '/' + f + '-jpeg.pdf')
-            print('ocrmypdf')
-            os.system('ocrmypdf -f --tesseract-pagesegmode 6 --jpeg-quality 100 ' + nombre_carpeta_jpeg_pdf + '/' + f + '-jpeg.pdf ' + nombre_carpeta_pdf_searchable + '/' + f + '-jpeg-ocr.pdf -l spa')
-            print('tabula')
-            tabula.convert_into(nombre_carpeta_pdf_searchable + "/" + f + "-jpeg-ocr.pdf", nombre_carpeta_tabula_csv_out + "/" + f + ".csv", output_format="csv", pages='all')
-            print('final')
-            leer_csv(nombre_pdf + '/' + f)
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        os.system(f'pdfseparate ./pdfs/{nombre_pdf}.pdf {nombre_carpeta_pdf_parts}/part-%d')
+        parts_list = os.listdir(nombre_carpeta_pdf_parts)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # Para ordenar los nombres de las partes en orden "natural" ascendente
+        parts_list = natsorted(parts_list)
+        df_list = []
+        indice = 1
+        for nombre_pdf_part in parts_list:
+            print(f'Procesando parte: {nombre_pdf_part}\n')
+
+            print('pdftoppm')
+            os.system(f'pdftoppm -singlefile -jpeg -r 300 -jpegopt quality=100 {nombre_carpeta_pdf_parts}/{nombre_pdf_part} {nombre_carpeta_jpeg}/{nombre_pdf_part}')
+
+            print('img2pdf')
+            os.system(f'img2pdf {nombre_carpeta_jpeg}/{nombre_pdf_part}.jpg -o {nombre_carpeta_jpeg_pdf}/{nombre_pdf_part}-jpeg.pdf')
+
+            print('ocrmypdf')
+            os.system(f'ocrmypdf -f --tesseract-pagesegmode 6 --jpeg-quality 100 -l spa {nombre_carpeta_jpeg_pdf}/{nombre_pdf_part}-jpeg.pdf {nombre_carpeta_pdf_searchable}/{nombre_pdf_part}-jpeg-ocr.pdf')
+
+            print('tabula')
+            df_list += extraer_datos(f'{nombre_carpeta_pdf_searchable}/{nombre_pdf_part}-jpeg-ocr.pdf', indice)
+            indice += 1
+
+        print("csv final")
+        imprimir_csv(f'{nombre_carpeta_csv_final}/{nombre_pdf}.csv', df_list)
+        end = time.time()
+        print(f"El tiempo transcurrido en minutos: {(end - start) / 60}")
+
+if __name__ == '__main__':
+    start = time.time()
+    procesar_archivos()
+    end = time.time()
+    print(f"El tiempo transcurrido total en minutos: {(end - start) / 60}")
